db5f061c37fb969d2285f2f73a53103f580c9182
Wesley Kibagendi <wesoon98@gmail.com>
2023-04-10 21:41:50 +0300

    defined and added footer template to the base layout.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -15,12 +15,18 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
+	//initializing slice containing the paths to the two files
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
 
 	// Use the template.ParseFiles() function to read the template file into a
 	// template set. If there's an error, we log the detailed error message and use
 	// the http.Error() function to send a generic 500 Internal Server Error
 	// response to the user.
-	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
